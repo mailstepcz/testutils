d299d93a5747afe1ddfb501d72af7f369c094cb5
Petr Homola <homola.p@gmail.com>
2025-02-07 23:42:39 +0000

    Struct tag checker (#4)
    * struct tag checker
    
    * fixed field name component case
    
    * fixed field name component case
    
    * PR comments

---
+++ structtags_test.go
@@ -0,0 +1,55 @@
+package testutils
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJSONTagChecker(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		req := require.New(t)
+
+		type person struct {
+			FullName   string      `json:"fullName"`
+			Age        int         `json:"age"`
+			JobID      uuid.UUID   `json:"jobId"`
+			AddressIDs []uuid.UUID `json:"addressIds"`
+		}
+
+		err := CheckJSONTags(reflect.TypeFor[person](), "id")
+		req.NoError(err)
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		req := require.New(t)
+
+		type person struct {
+			FullName   string      `json:"fullname"`
+			Age        int         `json:"age"`
+			JobID      uuid.UUID   `json:"jobId"`
+			AddressIDs []uuid.UUID `json:"addressIds"`
+		}
+
+		err := CheckJSONTags(reflect.TypeFor[person](), "id")
+		req.NotNil(err)
+		req.Contains("non-matching field 'fullName' ('fullName' <> 'fullname')", err.Error())
+	})
+
+	t.Run("failure in abbreviation", func(t *testing.T) {
+		req := require.New(t)
+
+		type person struct {
+			FullName   string      `json:"fullName"`
+			Age        int         `json:"age"`
+			JobID      uuid.UUID   `json:"jobId"`
+			AddressIDs []uuid.UUID `json:"addressIds"`
+		}
+
+		err := CheckJSONTags(reflect.TypeFor[person]())
+		req.NotNil(err)
+		req.Contains("non-matching field 'addressIDs' ('addressIDs' <> 'addressIds')", err.Error())
+	})
+}
